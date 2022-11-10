a1b145a0a7ff972beca0b5db08cf6c79a4b61e8e
Vladislave <fadeeverynight@gmail.com>
2022-11-10 11:51:51 +0300

    create first repo

--- script.js
+++
@@ -1,8 +0,0 @@
-let title = "GLO_Academy";
-let screens = true;
-const screenPrice = null;
-let rollback = 10n;
-const fullPrice = '15 eu';
-let adaptive = false;
-alert(title);
-console.log(typeof(rollback));
