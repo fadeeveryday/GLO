516b68048a48a65d13642f0850f62b16fa7653fc
Vladislave <fadeeverynight@gmail.com>
2022-11-10 12:06:58 +0300

    added vars on js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 let title = 'local page';
+let screens = true;
+const screenPrice = 10n;
+let rollback = null;
+const fullPrice = 123;
+let adaptive = Symbol;
+console.log(adaptive);
+alert(typeof(rollback));
